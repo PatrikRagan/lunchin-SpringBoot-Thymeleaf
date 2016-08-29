93062c9d1119fa369ae43747bdbc191d35f79e51
Patrik Ragan <ragan.patres@gmail.com>
2016-08-30 01:28:09 +0200

    adding users

--- src/main/java/com/patres/controller/UserController.java
+++ src/main/java/com/patres/controller/UserController.java
@@ -7,9 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * Created by Patres on 29. 8. 2016.
@@ -32,4 +35,30 @@ public class UserController {
         return new ModelAndView("redirect:/users");
     }
 
+    @RequestMapping(value = "/users/new", method = GET)
+    public String newUser() {
+        return "users/new";
+    }
+
+    @RequestMapping(value = "/users/create", method = POST)
+    public ModelAndView createUser(@RequestParam("firstName") String firstName) {
+        userRepository.save(new User(firstName, "byREST"));
+        return new ModelAndView("redirect:/users");
+    }
+
+    @RequestMapping(value = "/users/update", method = POST)
+    public ModelAndView updateUser(@RequestParam("user_id") long id,
+                                   @RequestParam("firstName") String firstName) {
+        User user = userRepository.findOne(id);
+        user.setFirstName(firstName);
+        userRepository.save(user);
+        return new ModelAndView("redirect:/users");
+    }
+
+    @RequestMapping(value = "/{id}/edit", method = RequestMethod.GET)
+    public String editUser(@PathVariable long id, Model model){
+        User user = userRepository.findOne(id);
+        model.addAttribute("user", user);
+        return "users/edit";
+    }
 }
